fa1e398cbb8bf38d93a705302ceb0b0dff4b0651
yoyowazzap <danieltmetro@gmail.com>
2025-03-03 14:38:34 -0700

    added grouped items map and regional maps

--- scripts/layouts.lua
+++ scripts/layouts.lua
@@ -7,4 +7,5 @@ if (string.find(Tracker.ActiveVariantUID,"standard")) then
     Tracker:AddLocations("locations/locations.json")
     Tracker:AddLayouts("layouts/broadcast.json")
     Tracker:AddLayouts("layouts/tracker.json")
+    Tracker:AddLayouts("layouts/maps.json")
 end
